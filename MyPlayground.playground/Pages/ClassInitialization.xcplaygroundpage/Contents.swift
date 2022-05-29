118d92f278f03dd10ff3afbca05eddf9d598c6c4
hackzfy <hackzfy@foxmail.com>
2022-05-29 22:13:49 +0800

    add demo code.

--- MyPlayground.playground/Pages/ClassInitialization.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/ClassInitialization.xcplaygroundpage/Contents.swift
@@ -59,7 +59,55 @@ class Child1: ParentWithDefaultInit {
  1. 调用构造器。
  2. 给实例分配内存空间，但是空间还没有初始化。（根据属性类型，分配空间，但是空间中没有赋值）
  3. 构造器确认所有属性已经初始化完成。存储这些属性的内存空间现在初始化了。（存储空间赋值）
- 4. 调用超类构造器，重复以上过程。
- 5. 
+ 4. 调用超类构造器，重复以上过程。直到继承链的顶端。
+ 5. 第一阶段完成。
+ 
+ 第二阶段：
+ 1. 从继承链顶端，自上而下，每一个传统构造器现在可以对实例进行配置。构造器现在可以访问 self 并修改属性值，调用实例方法等。
+ 2. 最终，任何便捷构造器可以配置实例属性冰使用 self。
  */
+
+
+class TestParent {
+    var name: String
+    
+    init(name: String) {
+        self.name = name;
+    }
+    
+    func description() -> String {
+        return "Test \(name)"
+    }
+}
+
+class TestChild: TestParent {
+    var age: Int
+    var gender = "unknown"
+    
+    
+    init(name: String, age: Int, gender: String) {
+        // init store property first
+        self.age = age
+        self.gender = gender
+        // invoke parent initialization
+        super.init(name: name)
+        // modify inherited property
+        self.name = "Child \(name)";
+    }
+    
+    convenience init() {
+        self.init(name: "fakeName", age: 0, gender: "unknown")
+    }
+    
+    override func description() -> String {
+        return "Test \(name) \(age) \(gender)"
+    }
+}
+
+
+var child = TestChild()
+child.description()
+
+child = TestChild(name: "John", age: 20, gender: "male")
+child.description()
 //: [Next](@next)
