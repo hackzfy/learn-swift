4a18cf4c3ce1c492e6ed0428e7d95feb621ae557
hackzfy <hackzfy@foxmail.com>
2022-05-30 10:58:01 +0800

    willSet and didSet

--- MyPlayground.playground/Pages/Basic2.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Basic2.xcplaygroundpage/Contents.swift
@@ -66,7 +66,7 @@ class EquilateralTriangle: NamedShape {
             return 3.0 * sideLength
         }
         set {
-            // newValue is the newly set value
+            // the new value has the implicit name newValue.
             sideLength = newValue / 3.0
         }
     }
@@ -80,6 +80,30 @@ triangle.perimeter = 9.9
 print(triangle.sideLength) // 3.3
 
 
+/**
+ willSet and didSet
+ */
+
+class WillAndDid {
+    
+    var name: String {
+       
+       // notice: you can't write willSet and didSet along with a getter.
+        willSet {
+            print("name is \(name) and will be set to \(newValue)")
+        }
+        didSet {
+            print("name has been set to \(self.name)")
+        }
+    }
+    init(name: String){
+        self.name = name
+        print("name initialized to \(name)")
+    }
+}
+
+let willAndDid = WillAndDid(name: "test");
+willAndDid.name = "done"
 
 
 //: [Next](@next)
